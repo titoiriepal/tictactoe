e5d95780d475bd5308bdf1dd24e5baa6e82e9a0a
DESKTOP-S642E9F\Pedro <titoiriepal@gmail.com>
2021-06-02 19:44:35 +0200

    Comprobación de valores de entrada de usuario

--- main.py
+++ main.py
@@ -1,98 +1,140 @@
 import os
 
+
 class Game:
+
+    WINNERMOVES = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
+                   (0, 3, 6), (1, 4, 7), (2, 5, 8),
+                   (0, 4, 8), (2, 4, 6)]
+
     def __init__(self):
-        self.breed = 'Game'
-    
+        self.board = Board()
+        self.board.displayBoard()
+        self.playGame(self.board)
+
     def playGame(self, board):
         turn = 1
         while True:
             move = 'a'
+            player = Player(turn)
+            board.printFreeMoves()
             while move not in (1, 2, 3, 4, 5, 6, 7, 8, 9):
-                player = Player()
                 move = player.chooseMove(turn)
-                if board.board[move - 1] != 0:
-                    print('El número elegido ya esta ocupado, intentalo de nuevo')
+                
+                if move < 1 or move > 9:
+                    print('El número elegido no está en rango, por favor elige un número de la tabla superior')
                     move = 'a'
-            board.board = self.assignTab(turn, move, board.board)
+                else:
+                    if board.board[move - 1] != 0:
+                        print('El número elegido ya esta ocupado, intentalo de nuevo')
+                        move = 'a'
+
+            board = self.assignTab(turn, move, board)
             board.displayBoard()
             self.checkWin(board)
             if turn == 9:
                 print('Ha sido un empate')
                 exit()
             turn = turn + 1
-        
+
     def assignTab(self, turn, move, board):
         if turn % 2 == 1:
-            board[move - 1] = 1
+            board.board[move - 1] = 1
         else:
-            board[move - 1] = -1
+            board.board[move - 1] = -1
         return board
 
-    def checkWin(self,board):
-        for a, b, c in [(0, 1, 2), (3, 4, 5), (6, 7, 8),
-                        (0, 3, 6), (1, 4, 7), (2, 5, 8),
-                        (0, 4, 8), (2, 4, 6)]:
+    def checkWin(self, board):
+        for a, b, c in self.WINNERMOVES:
             if board.board[a] + board.board[b] + board.board[c] == 3:
                 print('Gana el jugador 1')
                 exit()
             if board.board[a] + board.board[b] + board.board[c] == -3:
                 print('Gana el jugador 2')
                 exit()
-            
-
-
-
-
-
-
-
-
 
 
 class Board:
-    def __init__(self):
-        self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+    PLAYERVALUES = (' ', 'X', 'O')
+    GAMEVALUES = (0, 1, -1)
+
+    def __init__(self):
+        self.tablero = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.board = self.rellenaTabla()
+
+    def rellenaTabla(self):
+        # https://blog.teamtreehouse.com/python-single-line-loops
+        # https://stackoverflow.com/questions/716477/join-list-of-lists-in-python
+        return [j for i in self.tablero for j in i]
 
     def displayBoard(self):
         os.system("cls")
-        playerValues = (' ', 'X', 'O')
-        gameValues = (0, 1, -1)
+        self.fillTablero()
         for i in range(0, 3):
             print('   |   |   ')
-            indx1 = gameValues.index(self.board[i*3])
-            indx2 = gameValues.index(self.board[(i*3)+1])
-            indx3 = gameValues.index(self.board[(i*3)+2])
-            print(f' {playerValues[indx1]} | {playerValues[indx2]} | {playerValues[indx3]} ')
-            if i !=2:
+            indx1 = self.GAMEVALUES.index(self.board[i*3])
+            indx2 = self.GAMEVALUES.index(self.board[(i*3)+1])
+            indx3 = self.GAMEVALUES.index(self.board[(i*3)+2])
+            print(f' {self.PLAYERVALUES[indx1]} | {self.PLAYERVALUES[indx2]} | {self.PLAYERVALUES[indx3]} ')
+            if i != 2:
                 print('___|___|___')
             else:
                 print('   |   |   ')
-            
 
-    
+    def printFreeMoves(self):
 
+        def getFill(displacement=0):
+            table = []
+            for i in range(0, 3):
+                pos = (displacement * 3) + i
+                if self.board[pos] != 0:
+                    character = " "
+                else:
+                    character = str(pos + 1)
+                table.append(character)
+            return table
+
+        for i in range(0, 3):
+            a = getFill(i)
+            if i != 2:
+                print(f'          ' + "\033[4;37m" + f'{a[0]}|{a[1]}|{a[2]}' + '\033[0;37m')
+            else:
+                print(f'          {a[0]}|{a[1]}|{a[2]}')
+
+    def fillTablero(self):
+        for i in range(0, 3):
+            self.tablero[i][0] = self.board[i*3]
+            self.tablero[i][1] = self.board[(i*3)+1]
+            self.tablero[i][2] = self.board[(i*3)+2]
 
 
 class Player:
-    def __init__(self):
-        self.breed = 'Player'
+
+    CHARACTERA = 'X'
+    CHARACTERB = '0'
+
+    def __init__(self, turn):
+        if turn % 2 != 0:
+            self.mark = self.CHARACTERA
+        else:
+            self.mark = self.CHARACTERB
 
     def chooseMove(self, turn):
-        if turn % 2 == 1:
-            msg = 'Jugador 1'
-        else:
-            msg = 'Jugador 2'
-        move = input(f'{msg}, por favor introduce tu movimento o pulsa q para salir: ').lower()
-        if move == 'q':
-            exit()
-        return int(move)
+        currPlayer = '1' if turn % 2 else '2'
+        msg = 'Jugador ' + currPlayer
+        move = None
+        while move is None:
+            move = input(f'{msg}, por favor introduce tu movimento o pulsa q para salir: ').lower()
+            if move == 'q':
+                exit()
+            try:
+                move = int(move)
+            except ValueError:
+                print('Por favor, introduce un número de la tabla superior')
+                move = None
 
-    
+        return move
 
-b = Board()
-b.displayBoard()
-g = Game()
-g.playGame(b)
-    
+
+Game()
